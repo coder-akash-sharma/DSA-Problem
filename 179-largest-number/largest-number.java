43d1da23fa1af4bc9e35b1f704f5d2f97a00d597
Akash Sharma <ak2552sharma@gmail.com>
2026-08-22 00:03:59 +0530

    Time: 6 ms (94.71%) | Memory: 45.1 MB (29.24%) - LeetSync

---
+++ 179-largest-number/largest-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String largestNumber(int[] nums) {
+        String[] arr = new String[nums.length];
+         
+        // convert to string once 
+        for ( int i = 0 ; i < nums.length; i++){
+            arr[i] = String.valueOf(nums[i]);
+        }
+         
+         // comparator :
+         // a comes before b if ( a+b) is lexicographically larger tha (b+a).
+
+        Arrays.sort(arr,(a,b)->(b+a).compareTo(a+b));
+
+        // All zero case.
+        if (arr[0].equals("0")) return "0";
+
+        StringBuilder sb= new StringBuilder();
+        for (String s : arr) sb.append(s);
+        return sb.toString();
+        
+
+        
+    }
+}
